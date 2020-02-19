59a0de7c79756b3f905aeea9fb474292eebf9d5f
Troy Neubauer <troyneubauer@gmail.com>
2020-02-19 06:49:40 -0500

    Libusb code now prints only valid devices

--- src/main/MainLayer.cpp
+++ src/main/MainLayer.cpp
@@ -11,45 +11,57 @@ void MainLayer::OnAttach()
 	libusb_device **devs;
 	int r;
 	ssize_t cnt;
+	libusb_context* context;
 
-	r = libusb_init(NULL);
+	r = libusb_init(&context);
 	if (r < 0)
 		return;
 
-	cnt = libusb_get_device_list(NULL, &devs);
+	cnt = libusb_get_device_list(context, &devs);
 	if (cnt < 0){
-		libusb_exit(NULL);
+		libusb_exit(context);
 		return;
 	}
 
 	libusb_device *dev;
 	int i = 0, j = 0;
-	uint8_t path[8]; 
+	uint8_t path[8];
 
 	while ((dev = devs[i++]) != NULL) {
 		struct libusb_device_descriptor desc;
 		int r = libusb_get_device_descriptor(dev, &desc);
-		if (r < 0) {
-			fprintf(stderr, "failed to get device descriptor");
-			return;
+		if (r != LIBUSB_SUCCESS) {
+			continue;
 		}
 		
-		printf("%x:%x (bus %d, device %d)",
-			desc.idVendor, desc.idProduct,
-			libusb_get_bus_number(dev), libusb_get_device_address(dev));
-
-		r = libusb_get_port_numbers(dev, path, sizeof(path));
-		if (r > 0) {
-			printf(" path: %d", path[0]);
-			for (j = 1; j < r; j++)
-				printf(".%d", path[j]);
+		libusb_device_handle* handle;
+		r = libusb_open(dev, &handle);
+		if (r != LIBUSB_SUCCESS)
+		{
+			continue;
 		}
+
+		//XBee is: 0403:6001
+		unsigned char buf[512];
+		printf("VendorID:ProductID -> %02x:%02x (bus %d, device %d)", desc.idVendor, desc.idProduct, libusb_get_bus_number(dev), libusb_get_device_address(dev));
+
+		r = libusb_get_string_descriptor_ascii(handle, desc.idVendor, buf, sizeof(buf));
+		printf(", Vendor String: %s (returned %d)", buf, r);
+
+		r = libusb_get_string_descriptor_ascii(handle, desc.idProduct, buf, sizeof(buf));
+		printf(", Product String: %s (returned %d)", buf, r);
+
+		r = libusb_get_string_descriptor_ascii(handle, desc.iManufacturer, buf, sizeof(buf));
+		printf(", Manfacturer String: %s (returned %d)", buf, r);
+
+		libusb_close(handle);
+
 		printf("\n");
 	}
 
 	libusb_free_device_list(devs, 1);
 
-	libusb_exit(NULL);
+	libusb_exit(context);
 }
 
 void MainLayer::OnDetach()
