390e1e796d5da3bd080cf567c47ed33d630da1bf
Troy Neubauer <troyneubauer@gmail.com>
2020-02-21 07:44:43 -0500

    Added pre compiled header

---
+++ src/gspch.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <Hazel.h>
+
+#include <vector>
+#include <string>
+#include <utility>
+
+#ifdef HZ_PLATFORM_WINDOWS
+
+#include <Windows.h>
+
+//Other includes
+#include <tchar.h>
+#include <setupapi.h>
+#include <winspool.h>
+#include <Wbemcli.h>
+#include <comdef.h>
+#include <stdio.h>
+
+
+#endif
